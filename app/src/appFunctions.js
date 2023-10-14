a5f562d6ffe6b756be04b56087e61c8a372f9523
MigueldeHaroce <migueldeharoce@gmail.com>
2023-10-15 01:59:51 +0200

    finished front end

--- app/src/appFunctions.js
+++ app/src/appFunctions.js
@@ -15,3 +15,26 @@ function minimize_app() {
 document.addEventListener("DOMContentLoaded", () => {
   document.body.classList.add("loaded");
 });
+
+
+
+document.getElementById('aux1').addEventListener('change', function() {
+  var mainContainer = document.getElementById('mainContainer');
+  mainContainer.style.transition = "all 0.5s ease";
+  var searchBar = document.getElementById('searchBar');
+
+  if(this.checked) {
+    document.body.style.backgroundColor = '#101010';
+    mainContainer.style.backgroundColor = '#c4302b';
+    mainContainer.style.borderStyle = 'solid';
+    mainContainer.style.borderWidth = '2px';
+    mainContainer.style.borderColor = '#fff';
+    searchBar.placeholder = "Yt link to donwload...";
+
+  } else {
+    document.body.style.backgroundColor = '#000';
+    mainContainer.style.backgroundColor = 'rgb(24, 94, 82)';
+    mainContainer.style.borderStyle = 'none';
+    searchBar.placeholder = "Playlist link to donwload...";
+  }
+});
